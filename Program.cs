d660d03fac7436f42e87c4e7c81329e4d3476243
Damir <mr.damirich@mail.ru>
2023-05-25 23:00:19 +0500

    Domashka

---
+++ Program.cs
@@ -0,0 +1,105 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, 
+//какое число большее, а какое меньшее.
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+// Console.WriteLine("Введите число");
+// int num1=Convert.ToInt32 ( Console.ReadLine());
+
+// Console.WriteLine("Введите число");
+// int num2=Convert.ToInt32 ( Console.ReadLine());
+// if(num1>num2)
+// {
+//     Console.WriteLine("наибольшее число:"+num1);
+//     Console.WriteLine("наименьшее число:"+num2);
+// }
+// else
+// {
+//     Console.WriteLine("наибольшее число:"+num2);
+//     Console.WriteLine("наименьшее число:"+num1);
+// }
+
+//_______________________________________________________
+
+// Напишите программу, которая принимает на вход три 
+// числа и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+// Console.WriteLine("Введите число");
+// int num1=Convert.ToInt32 ( Console.ReadLine());
+
+// Console.WriteLine("Введите число");
+// int num2=Convert.ToInt32 ( Console.ReadLine());
+
+// Console.WriteLine("Введите число");
+// int num3=Convert.ToInt32 ( Console.ReadLine());
+// int max=num1;
+// if (num2 > max ) max = num2;
+// if (num3>max) max=num3;
+
+// Console.WriteLine("Максимальное число:"+ max);
+
+//__________________________________________________________________
+
+// Напишите программу, которая на вход принимает число и выдаёт, 
+// является ли число чётным (делится ли оно на два без остатка).
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+// Console.WriteLine("Введите число");
+// int num=Convert.ToInt32 ( Console.ReadLine());
+
+// if(num%2==0)
+
+// {
+//     Console.WriteLine("Это число чётное");
+// }
+
+//_________________________________________________________________________
+
+//  Напишите программу, которая принимает на вход цифру,
+//  обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+// Console.WriteLine("Введите число");
+// int num=Convert.ToInt32 ( Console.ReadLine());
+
+
+// if(num==1)
+// {
+//     Console.WriteLine("нет");
+// }
+// if(num==2)
+// {
+//     Console.WriteLine("нет");
+// }
+// if(num==3)
+// {
+//     Console.WriteLine("нет");
+// }
+// if(num==4)
+// {
+//     Console.WriteLine("нет");
+// }
+// if(num==5)
+// {
+//     Console.WriteLine("нет");
+// }
+// if(num==6)
+// {
+//     Console.WriteLine("да");
+// }
+// if(num==7)
+// {
+//     Console.WriteLine("да");
+// }
+// else
+// {
+//     Console.WriteLine("такого дня нет");
+// }
